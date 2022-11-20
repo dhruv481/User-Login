cdf5ce437058ae0cde1864fd2629372b0cdc7691
dhruv481 <dguptadg01@gmail.com>
2022-11-20 23:04:52 +0530

    Optimize Navigation.js

--- src/components/MainHeader/Navigation.js
+++ src/components/MainHeader/Navigation.js
@@ -1,13 +1,15 @@
-import React from "react";
+import React, { useContext } from "react";
 import AuthContext from "../../store/auth-context";
 
 import classes from "./Navigation.module.css";
 
 const Navigation = (props) => {
+  const ctx = useContext(AuthContext);
+
   return (
-    <AuthContext.Consumer>
-      {(ctx) => {
-        return (
+    // <AuthContext.Consumer>
+    //   {(ctx) => {
+    //     return (
           <nav className={classes.nav}>
             <ul>
               {ctx.isLoggedIn && (
@@ -27,9 +29,9 @@ const Navigation = (props) => {
               )}
             </ul>
           </nav>
-        );
-      }}
-    </AuthContext.Consumer>
+    //     );
+    //   }}
+    // </AuthContext.Consumer>
   );
 };
 
